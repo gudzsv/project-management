456d2e306bac4a74b5045c97fe93bc4314593cf4
Serhii Hudzenko <gudzsvmail@gmail.com>
2025-01-13 14:58:03 +0100

    added reusable button, added logic between new project and noProject also added handlers to buttons

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
-export default function Input({ textarea, label, ...props }) {
+const Input = forwardRef(function Input({ textarea, label, ...props }, ref) {
 	const classes =
 		'w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600';
 
@@ -10,10 +10,12 @@ export default function Input({ textarea, label, ...props }) {
 				{label}
 			</label>
 			{textarea ? (
-				<textarea className={classes} {...props} />
+				<textarea ref={ref} className={classes} {...props} />
 			) : (
-				<input className={classes} {...props} />
+				<input ref={ref} className={classes} {...props} />
 			)}
 		</p>
 	);
-}
+});
+
+export default Input;
