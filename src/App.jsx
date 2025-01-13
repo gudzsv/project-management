14d5ddc2e1693eeee909f08dd89f2a8074783af0
Serhii Hudzenko <gudzsvmail@gmail.com>
2025-01-13 20:49:29 +0100

    Add validation input and error modal

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,17 @@ function App() {
 
 			return {
 				...prevState,
+				selectedProjectId: undefined,
 				projects: [...prevState.projects, newProject],
+			};
+		});
+	}
+
+	function handleCancelAddProject() {
+		setProjectsState((prevState) => {
+			return {
+				...prevState,
+				selectedProjectId: undefined,
 			};
 		});
 	}
@@ -35,14 +45,19 @@ function App() {
 	let content;
 
 	if (projectsState.selectedProjectId === null) {
-		content = <NewProject onAdd={handleAddProject} />;
+		content = (
+			<NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+		);
 	} else if (projectsState.selectedProjectId === undefined) {
 		content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
 	}
 
 	return (
 		<main className='h-screen my-8 flex gap-8'>
-			<ProjectsSidebar onStartAddProject={handleStartAddProject} />
+			<ProjectsSidebar
+				onStartAddProject={handleStartAddProject}
+				projects={projectsState.projects}
+			/>
 			{content}
 		</main>
 	);
